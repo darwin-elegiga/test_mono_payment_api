9ce33db9e47a059f6c1e9fa79cbb8b7a22acda0c
Chris Hammons <chammons@vpayusa.com>
2018-08-08 17:12:30 -0500

    Switching to linux container

--- PaymentWebAPI/Program.cs
+++ PaymentWebAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PaymentWebAPI
+namespace PaymentWebApi
 {
     public class Program
     {
