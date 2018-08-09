34a73bd05858c80a6f4220c54b776636dbbeba9f
Chris Hammons <chammons@vpayusa.com>
2018-08-09 10:53:38 -0500

    PROD-1675 - Including nuget packages from other projects; configuring application and graylog for multiple environments

--- VPay.Payment.Api/Program.cs
+++ VPay.Payment.Api/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Gelf.Extensions.Logging;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace VPay.Payment.Api
 {
@@ -12,6 +17,29 @@ namespace VPay.Payment.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, configurationBuilder) =>
+                {
+                    IHostingEnvironment hostingEnvironment = builderContext.HostingEnvironment;
+
+                    configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.Local.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.secure.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureLogging((builderContext, loggingBuilder) =>
+                {
+                    IConfigurationSection graylogSection = builderContext.Configuration.GetSection("Graylog");
+                    loggingBuilder.Services.Configure<GelfLoggerOptions>(graylogSection);
+
+                    loggingBuilder.Services.PostConfigure<GelfLoggerOptions>(gelfLoggerOptions =>
+                        gelfLoggerOptions.AdditionalFields["machine_name"] = Environment.MachineName);
+
+                    IConfigurationSection loggingSection = builderContext.Configuration.GetSection("Logging");
+                    loggingBuilder.AddConfiguration(loggingSection)
+                        .AddConsole()
+                        .AddDebug()
+                        .AddGelf();
+                })
                 .UseStartup<Startup>();
     }
 }
--- VPay.Payment.Api/Startup.cs
+++ VPay.Payment.Api/Startup.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using GlobalExceptionHandler.WebApi;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VPay.Payment.Api.Validation;
 
 namespace VPay.Payment.Api
 {
@@ -14,22 +19,99 @@ namespace VPay.Payment.Api
         }
 
         public IConfiguration Configuration { get; }
+        private ILogger _logger;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc(opt =>
+                {
+                    opt.Filters.Add(typeof(ValidatorActionFilter));
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonSettings()
+                .AddFluentValidationSettings();
+
+            services
+                .AddApiVersioningService()
+                .AddOptions()
+                .AddSwaggerGenService();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
-            app.UseMvc();
+            app.UseExceptionHandler("/error").WithConventions(x => {
+                ConfigureExceptionHandler(x, env.IsDevelopment());
+            });
+
+            app.Map("/error", x => x.Run(y => throw new Exception()));
+
+            app.UseMvc()
+                .UseSwagger();
+
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                //The version after /swagger/ in the URL must match the "name" established when defining the API documentation in calls to SwaggerDoc
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Payment v1");
+            });
+
+            _logger = loggerFactory.CreateLogger<Startup>();
+
+
+            appLifetime.ApplicationStarted.Register(OnStartup);
+            appLifetime.ApplicationStopping.Register(OnShutdown);
+            appLifetime.ApplicationStopped.Register(OnShutdownComplete);
+        }
+
+
+        private void OnStartup()
+        {
+            _logger.LogInformation("Startup occurred for VPay.Payment.Api");
+        }
+
+        private void OnShutdown()
+        {
+            _logger.LogInformation("Shutdown occurred for VPay.Payment.Api");
+        }
+
+        private void OnShutdownComplete()
+        {
+            _logger.LogInformation("Shutdown completed for VPay.Payment.Api");
+        }
+
+
+        private void ConfigureExceptionHandler(ExceptionHandlerConfiguration config, bool isDevelopment)
+        {
+            config.ContentType = "application/json";
+
+            if (isDevelopment)
+            {
+                config.MessageFormatter(s => JsonConvert.SerializeObject(new
+                {
+                    message = s.Message,
+                    exceptionType = s.GetType().FullName,
+                    stacktrace = s.StackTrace,
+                    innerException = s.InnerException == null ? null : new
+                    {
+                        message = s.InnerException.Message,
+                        exceptionType = s.InnerException.GetType().FullName,
+                        stackTrace = s.InnerException.StackTrace
+                    }
+                }));
+            }
+            else
+            {
+                config.MessageFormatter(s => JsonConvert.SerializeObject(new
+                {
+                    message = "An error occurred while processing your request"
+                }));
+            }
         }
     }
 }
