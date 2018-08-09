a6ecfaa7dc368fe4dbf85e6e3543624ccb2d7a38
Chris Hammons <chammons@vpayusa.com>
2018-08-09 09:36:47 -0500

    PROD-1675 - Renaming solution and project to standardize on conventions

--- VPay.Payment.Api/Controllers/StartController.cs
+++ VPay.Payment.Api/Controllers/StartController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace PaymentWebAPI.Controllers
+namespace VPay.Payment.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
