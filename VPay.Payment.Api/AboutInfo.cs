c456ce3e4a1038d367cba54727b6561b367a2cf8
Chris Hammons <chammons@vpayusa.com>
2018-08-10 14:06:39 -0500

    PROD-1676 - Adding in About page

---
+++ VPay.Payment.Api/AboutInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace VPay.Payment.Api
+{
+    public class AboutInfo
+    {
+        // More build variables available upon request
+
+        public string BuildName { get; internal set; }
+        public string GitRevision { get; internal set; }
+        public string BuildTime { get; internal set; }
+        public string Environment { get; internal set; }
+        public string VersionInfo { get; internal set; }
+
+        #region For build process
+        // Do not modify this region without updating build plan
+        public static AboutInfo GetBuildAboutInfo()
+        {
+            AboutInfo infoForTheBuildPlanToModify = new AboutInfo();
+            infoForTheBuildPlanToModify.BuildName = "[[BuildName]]";
+            infoForTheBuildPlanToModify.GitRevision = "[[GitRevision]]";
+            infoForTheBuildPlanToModify.BuildTime = "[[BuildTime]]";
+            infoForTheBuildPlanToModify.Environment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            infoForTheBuildPlanToModify.VersionInfo = "[[VersionInfo]]";
+
+            return infoForTheBuildPlanToModify;
+        }
+
+        #endregion
+
+    }
+}
---
+++ VPay.Payment.Api/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VPay.Payment.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+
+        /// <summary>
+        /// About the build that created the service
+        /// </summary>
+        /// <returns>Information about the build that created the service</returns>
+        /// <response code="200">Information about the build that created the service</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(AboutInfo), 200)]
+        public AboutInfo GetDetail()
+        {
+            return AboutInfo.GetBuildAboutInfo();
+        }
+
+    }
+}
