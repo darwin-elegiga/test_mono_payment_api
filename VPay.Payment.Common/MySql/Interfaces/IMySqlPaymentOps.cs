230f3157e4d37f47f41f53bed64f561cc3d7448d
Chris Hammons <chammons@vpayusa.com>
2018-08-10 15:48:40 -0500

    PROD-1730 - Added in a way to connect to the MySql database named vpay.

---
+++ VPay.Payment.Common/MySql/Interfaces/IMySqlPaymentOps.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VPay.Payment.Common.MySql
+{
+    public interface IMySqlPaymentOps : IDisposable
+    {
+        
+    }
+}
---
+++ VPay.Payment.MySql/MySqlPaymentOps.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using VPay.Payment.Common;
+using VPay.Payment.Common.MySql;
+
+namespace VPay.Payment.MySql
+{
+    public class MySqlPaymentOps : IMySqlPaymentOps, IHealthCheck
+    {
+        private readonly MySqlConnectionConfig _connectionConfig;
+        private MySqlConnection _dbConnection;
+
+        public MySqlPaymentOps(MySqlConnectionConfig connectionConfig)
+        {
+            _connectionConfig = connectionConfig ?? throw new ArgumentNullException(nameof(connectionConfig));
+
+        }
+
+        public string Component { get; } = "MySql";
+
+        public async Task<bool> IsHealthy()
+        {
+            try
+            {
+                var connection = await GetOpenConnection();
+                return connection.State == ConnectionState.Open;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _dbConnection?.Close();
+            _dbConnection?.Dispose();
+        }
+
+        private async Task<MySqlConnection> GetOpenConnection()
+        {
+
+            if (_dbConnection == null)
+            {
+                _dbConnection = new MySqlConnection(ConvertToConnectionString(_connectionConfig));
+                await _dbConnection.OpenAsync();
+            }
+
+            return _dbConnection;
+        }
+
+        private string ConvertToConnectionString(MySqlConnectionConfig settings)
+        {
+            Enum.TryParse(settings.SslMode, out MySqlSslMode sslMode);
+
+            var sb = new MySqlConnectionStringBuilder
+            {
+                Server = settings.Hostname,
+                Port = settings.Port,
+                UserID = settings.UserName,
+                Password = settings.Password,
+                Database = settings.Database,
+                SslMode = sslMode,
+                CheckParameters = settings.CheckParameters
+            };
+            return sb.ConnectionString;
+        }
+
+    }
+}
