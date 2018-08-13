51883406314f788bb7cedca3621949e31690275f
Chris Hammons <chammons@vpayusa.com>
2018-08-13 14:15:42 -0500

    PROD-1729 - adjusting the template and settings to allow for multiple environments on deployment

--- VPay.Payment.Common/Db2/Models/Db2ConnectionConfig.cs
+++ VPay.Payment.Common/Db2/Models/Db2ConnectionConfig.cs
@@ -4,6 +4,8 @@
     {
         public string Dsn { get; set; }
 
+        public string Hostname { get; set; }
+
         public string UserName { get; set; }
 
         public string Password { get; set; }
