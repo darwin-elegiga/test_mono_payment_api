1d5dd9c8765fae24ba57e2084feea7f5090f6465
Jesse Marek <jmarek@vpayusa.com>
2018-08-31 11:40:09 -0500

    Generating stub code for further development and testing

--- VPay.Payment.Common/Interfaces/ITransactionService.cs
+++ VPay.Payment.Common/Interfaces/ITransactionService.cs
@@ -5,7 +5,19 @@ namespace VPay.Payment.Common
 {
     public interface ITransactionService
     {
+        Task<StandardResponse> GetReasonCodes(StandardRequest standardRequest);
+        Task<StandardResponse> GetTransactionDetails(StandardRequest standardRequest);
+        Task<StandardResponse> GetPanNumber(StandardRequest standardRequest);
+        Task<StandardResponse> OpenPreAuth(StandardRequest standardRequest);
+        Task<StandardResponse> LoadPan(StandardRequest standardRequest);
+        Task<StandardResponse> GetBalanceRequest(StandardRequest standardRequest);
+        Task<StandardResponse> UnloadPan(StandardRequest standardRequest);
+        Task<StandardResponse> StopPay(StandardRequest standardRequest);
+        Task<StandardResponse> CancelFax(StandardRequest standardRequest);
+        Task<StandardResponse> ChangeFaxNumber(StandardRequest standardRequest);
+        Task<StandardResponse> HoldFax(StandardRequest standardRequest);
+        Task<StandardResponse> ReleaseFax(StandardRequest standardRequest);
+        Task<StandardResponse> ResendFax(StandardRequest standardRequest);
 
-        Task<StandardResponse> GetBalanceRequest(StandardRequest sr);
     }
 }
