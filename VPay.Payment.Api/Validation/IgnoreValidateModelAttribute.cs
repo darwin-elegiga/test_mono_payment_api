3b14d41d26e4e7bd40cf7998833e966343580281
Chris Hammons <chammons@vpayusa.com>
2018-10-03 11:38:40 -0500

    PROD-2074 - adding back validation still needed; removing out code no longer needed

--- VPay.Payment.Api/Validation/IgnoreValidateModelAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-
-namespace VPay.Payment.Api.Validation
-{
-    public class IgnoreValidateModelAttribute : ActionFilterAttribute
-    {
-        
-    }
-}
--- VPay.Payment.Api/Validation/ValidatorActionFilter.cs
+++ VPay.Payment.Api/Validation/ValidatorActionFilter.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace VPay.Payment.Api.Validation
 {
@@ -8,9 +6,7 @@ namespace VPay.Payment.Api.Validation
     {
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var find = filterContext.ActionDescriptor.FilterDescriptors.Select(x => x.Filter).OfType<IgnoreValidateModelAttribute>().Any();
-
-            if (!find && !filterContext.ModelState.IsValid)
+            if (!filterContext.ModelState.IsValid)
             {
                 filterContext.Result = new ValidationFailedResult(filterContext.ModelState);
             }
