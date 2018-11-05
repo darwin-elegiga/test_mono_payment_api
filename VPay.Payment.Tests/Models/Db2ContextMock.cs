0ee0c43ab44943cf2641604664127c0d497745d5
Chris Hammons <chammons@vpayusa.com>
2018-11-05 16:09:14 -0600

    PROD-2479 - Adding in tests for the edge cases of service exceptions and default value setting

---
+++ VPay.Payment.Tests/Models/Db2ContextMock.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using VPay.Data.Db2.Abstractions;
+
+namespace VPay.Payment.Tests.Models
+{
+    public class Db2ContextMock : IDb2Context
+    {
+
+        public bool CanConnectMock { get; set; } = true;
+
+        public Task<bool> CanConnectAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return Task.FromResult(CanConnectMock);
+        }
+
+        public IFax Fax => FaxMock.Object;
+        public ISecurity Security => SecurityMock.Object;
+        public ITransactionWs TransactionWs => TransactionWsMock.Object;
+        public IPreferencing Preferencing => PreferencingMock.Object;
+        public ICorrespondenceRepo Correspondence => CorrespondenceMock.Object;
+
+        public Mock<IFax> FaxMock { get; } = new Mock<IFax>();
+        public Mock<ISecurity> SecurityMock { get; } = new Mock<ISecurity>();
+        public Mock<ITransactionWs> TransactionWsMock { get; } = new Mock<ITransactionWs>();
+        public Mock<IPreferencing> PreferencingMock { get; } = new Mock<IPreferencing>();
+        public Mock<ICorrespondenceRepo> CorrespondenceMock { get; } = new Mock<ICorrespondenceRepo>();
+    }
+}
