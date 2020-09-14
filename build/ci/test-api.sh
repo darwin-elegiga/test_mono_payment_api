50dd0d7d7c50e145415a179d94d0859d35e57ad0
Brandon Slade <bslade@vpayusa.com>
2020-09-14 15:57:19 -0500

    ci(gitlab): add GitLab pipeline and helm chart

--- build/ci/test-api.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname $0)
-
-set -e
-
-docker run --rm \
-    --workdir /dotnet/VPay.Payment.Api.Tests \
-    payment-api/build-sdk:${GIT_COMMIT_SHORT_HASH:-docker} \
-    dotnet test -c Debug
