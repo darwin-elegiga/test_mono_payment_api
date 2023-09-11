c15a5d1940945a0f5dd213db76e2c822ce9285fd
Michael Schmid <michael.schmid@optum.com>
2023-09-11 16:49:29 -0500

    another tests

--- VPay.Payment.Api/Auth/ServiceMethodHandler.cs
+++ VPay.Payment.Api/Auth/ServiceMethodHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
 using VPay.Payment.Common;
 
 namespace VPay.Payment.Api.Auth
@@ -13,14 +14,18 @@ namespace VPay.Payment.Api.Auth
     {
 
         private readonly IAuthService _authService;
+        private readonly ILogger<ServiceMethodHandler> _logger;
 
-        public ServiceMethodHandler(IAuthService authService)
+        public ServiceMethodHandler(IAuthService authService, ILogger<ServiceMethodHandler> logger)
         {
             _authService = authService;
+            _logger = logger;
         }
 
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ServiceMethodRequirement requirement)
         {
+            _logger.LogInformation("Authorization: Check Claims");
+
             // If the user does not have a name then we can not validate this requirement
             if (!context.User.HasClaim(c => c.Type == ClaimTypes.Name))
             {
@@ -29,12 +34,19 @@ namespace VPay.Payment.Api.Auth
 
             var userId = context.User.FindFirst(c => c.Type == ClaimTypes.Name).Value;
 
+            _logger.LogInformation($"Authorization: Is User {userId} authorized");
+
             var (permissionName, action) = requirement.Permission.GetPermissionInfo();
 
             if (await _authService.IsAuthorized(userId, permissionName, action))
             {
+
+                _logger.LogInformation($"Authorization: User {userId} Authorized!");
                 context.Succeed(requirement);
             }
+
+
+            _logger.LogInformation($"Authorization: User {userId} completed");
         }
     }
 }
