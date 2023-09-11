284526d3b57b0ed5e08dfcc86ca30ee0a39b94ed
Michael Schmid <michael.schmid@optum.com>
2023-09-11 17:13:38 -0500

    one more

--- VPay.Payment/AuthService.cs
+++ VPay.Payment/AuthService.cs
@@ -46,6 +46,8 @@ namespace VPay.Payment
 
         public async Task<UserSessionInfo> Login(AuthenticationParam param)
         {
+            _logger.LogInformation($"AuthService: Login: ValidateIP");
+
             if (_config.ValidateIP)
             {
                 var ipValidate = await TestAuthentication(param);
@@ -57,6 +59,9 @@ namespace VPay.Payment
 
             UserSessionInfo userSession = null;
 
+
+            _logger.LogInformation($"AuthService: Try Login for {param.UserId}");
+
             var loginTry = await DoLogin(param.UserId, param.Password, "WEBSERVICE");
 
             if (loginTry != null)
@@ -64,6 +69,9 @@ namespace VPay.Payment
                 userSession = loginTry;
                 userSession.Source = 'S';
             }
+
+
+            _logger.LogInformation($"AuthService: Completed Login for {param.UserId}");
 
             return userSession;
         }
@@ -78,6 +86,8 @@ namespace VPay.Payment
                 Action = action,
                 SecurityObjectName = secObjName
             });
+
+            _logger.LogInformation($"AuthService: IsAuthorized: Result:{result?.Result}, Description:{result?.Description}");
 
             return result?.Result == "0000";
         }
