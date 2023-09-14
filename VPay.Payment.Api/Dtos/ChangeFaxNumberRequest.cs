66995b862b00e3728e04c36b795a30de2a25ea35
Kunal Patel <kunal_patel@optum.com>
2023-09-14 15:37:39 -0400

    updating resendfaxrequest

--- VPay.Payment.Api/Dtos/ChangeFaxNumberRequest.cs
+++ VPay.Payment.Api/Dtos/ChangeFaxNumberRequest.cs
@@ -1,15 +1,16 @@
-﻿
-using System.Text.RegularExpressions;
+﻿using System;
 using Newtonsoft.Json;
 using VPay.Payment.Common;
-
 namespace VPay.Payment.Api.Dtos
 {
     public class ChangeFaxNumberRequest : FaxRequest
     {
+        //DeveloperNote: ChangeFaxNumberRequest is a parentClass for ResendFaxRequest so needs to copy below property has to move on Resendfaxrequest class
+        [Obsolete("Comment: This API endpoint is no longer used and has been deprecated")]
         public string FaxNumber { get; set; }
 
         [JsonIgnore]
+        [Obsolete("Comment: This API endpoint is no longer used and has been deprecated")]
         public string CleanFaxNumber => FaxNumber.CleanFaxNumber();
     }
 }
--- VPay.Payment.Api/Dtos/ResendFaxRequest.cs
+++ VPay.Payment.Api/Dtos/ResendFaxRequest.cs
@@ -1,7 +1,16 @@
-﻿namespace VPay.Payment.Api.Dtos
-{
-    public class ResendFaxRequest : ChangeFaxNumberRequest
-    {
+﻿using System;
+using VPay.Payment.Common;
+using Newtonsoft.Json;
 
+namespace VPay.Payment.Api.Dtos
+{
+    public class ResendFaxRequest : FaxRequest  //ChangeFaxNumberRequest
+    {
+        //DeveloperNote: ChangeFaxNumberRequest is a parentClass for ResendFaxRequest so needs to copy below property has to move on Resendfaxrequest class
+
+        public string FaxNumber { get; set; }
+
+        [JsonIgnore]
+        public string CleanFaxNumber => FaxNumber.CleanFaxNumber();
     }
 }
