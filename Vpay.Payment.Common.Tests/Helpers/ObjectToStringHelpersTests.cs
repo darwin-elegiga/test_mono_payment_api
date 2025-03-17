2f0649544ac5f4cd6f7645155e5ca69def47cb8d
Sravan Kumar Boyalakuntla <boyalakuntla_kumar+uhg@optum.com>
2025-03-17 22:05:12 +0530

    Updated Code for errors in Build

--- Vpay.Payment.Common.Tests/Helpers/ObjectToStringHelpersTests.cs
+++ Vpay.Payment.Common.Tests/Helpers/ObjectToStringHelpersTests.cs
@@ -912,7 +912,7 @@ namespace VPay.Payment.Common.Tests
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "CardInformation [ CardType=Visa, CardNumber=1234*********56, CardCvv2=***, CardExpiration=12/2023, LoadTransactionId=1, LoadAmount=$1.00, LoadFee=$1.00, PayeeName=name, CardholderName=name, CardholderAddress=address, CardholderPostalCode=postal ]\r\n";
+            var expected = "CardInformation [ CardType=Visa, CardNumber=1234*********56, CardCvv2=***, CardExpiration=12/2023, LoadTransactionId=1, LoadAmount=¤1.00, LoadFee=¤1.00, PayeeName=name, CardholderName=name, CardholderAddress=address, CardholderPostalCode=postal ]\r\n";
             Assert.Equal(expected, result);
         }
 
