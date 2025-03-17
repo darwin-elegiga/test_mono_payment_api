49178c1d15a2709a846bb47e07d6e70f0772f058
Sravan Kumar Boyalakuntla <boyalakuntla_kumar+uhg@optum.com>
2025-03-17 18:59:15 +0530

    Added new Test cases for ObjectToStringhelper Class

--- Vpay.Payment.Common.Tests/Helpers/ObjectToStringHelpersTests.cs
+++ Vpay.Payment.Common.Tests/Helpers/ObjectToStringHelpersTests.cs
@@ -246,14 +246,22 @@ namespace VPay.Payment.Common.Tests
             {
                 TransNumber = "123",
                 User = "testUser",
-                Token = "token"
+                Token = "token",
+                SeClaimID = "1234",
+                TpaClaimID = "test",
+                ProgramID = "program",
+                ReasonCode = "001",
+                ReasonDesc = "desc",
+                ResponseCode = "code",
+                SuccessCode = "success",
+                SuccessDesc = "success",
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "CommonData [ TransNumber=123, User=testUser, Token=**** ]\r\n";
+            var expected = "CommonData [ TransNumber=123, SeClaimId=1234, TpaClaimId=test, ProgramId=program, ReasonCode=001, ReasonDesc=desc, ResponseCode=code, SuccessCode=success, SuccessDesc=success, User=testUser, Token=**** ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -266,14 +274,26 @@ namespace VPay.Payment.Common.Tests
                 CardType = "Visa",
                 CardNumber = "1234567890123456",
                 CardCvv2 = "123",
-                CardExpiration = "12/2023"
+                CardExpiration = "12/2023",
+                LoadTransId = "1",
+                LoadAmount = "20",
+                LoadFee = "30",
+                PayeeName = "Test",
+                CardholderName = "Test",
+                CardholderAddress = "Test",
+                CardPostalCode = "Test",
+                UnloadCode = "Test",
+                UnloadDesc = "Test",
+                VcRef = "Test",
+                DisplayCVV2 = "Test",
+                MaskPan = "Test"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "CardData [ CardType=Visa, CardNumber=1234*********56, CardCvv2=1***, CardExpiration=12/2023 ]\r\n";
+            var expected = "CardData [ CardType=Visa, CardNumber=1234*********56, CardCvv2=1***, CardExpiration=12/2023, LoadTransId=1, LoadAmount=20, LoadFee=30, PayeeName=Test, CardholderName=Test, CardPostalCode=Test, UnloadCode=Test, UnloadDesc=Test, VcRef=Test, DisplayCVV2=Test, MaskPan=Test ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -284,14 +304,27 @@ namespace VPay.Payment.Common.Tests
             var entity = new CheckData
             {
                 CheckDate = "01/01/2023",
-                CheckNumber = "123456"
+                CheckNumber = "123456",
+                PosPayNumber = "123456",
+                SwitchNumber = "123456",
+                ChkNum1 = "123456",
+                ChkNum2 = "123456",
+                Address1 = "address1",
+                Address2 = "address2",
+                Address3 = "address3",
+                City = "city",
+                Country = "country",
+                Memo = "memo",
+                Region = "region",
+                StateOrProvince = "state",
+                Zip = "zip"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "CheckData [ CheckDate=01/01/2023, CheckNumber=123456 ]\r\n";
+            var expected = "CheckData [ CheckDate=01/01/2023, CheckNumber=123456, PosPayNumber=123456, SwitchNumber=123456, ChkNum1=123456, ChkNum2=123456, Address1=address1, Address2=address2, Address3=address3, City=city, Country=country, Memo=memo, Region=region, StateOrProvince=state, Zip=zip ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -302,7 +335,14 @@ namespace VPay.Payment.Common.Tests
             var entity = new ClaimData
             {
                 UserKey = "userKey",
-                UserField1 = "field1"
+                UserField1 = "field1",
+                UserField2 = "field2",
+                UserField3 = "field3",
+                ClaimDescription = "desc",
+                RequesterId = "id",
+                RequesterName = "name",
+                RepairOrderId = "orderId",
+                ClaimNotes = "notes"
             };
 
             // Act
@@ -320,14 +360,19 @@ namespace VPay.Payment.Common.Tests
             var entity = new CorrespondenceData
             {
                 AttachmentLocation = "location",
-                DocumentID = "docId"
+                DocumentID = "docId",
+                Email = "email",
+                FaxCode = "fax",
+                FaxStat = "stat",
+                PhoneNumber = "phone",
+                Type = "type"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "CorrespondenceData [ AttachmentLocation=location, DocumentID=docId ]\r\n";
+            var expected = "CorrespondenceData [ AttachmentLocation=location, DocumentID=docId, Email=email, FaxCode=fax, FaxStat=stat, PhoneNumber=phone, Type=type ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -338,14 +383,26 @@ namespace VPay.Payment.Common.Tests
             var entity = new CoveredItemData
             {
                 ItemType = "type",
-                ItemId = "id"
+                ItemId = "id",
+                Manufacturer = "Test",
+                Model = "Test",
+                BookStateOrProvince = "Test",
+                PostalCode = "Test",
+                PlanCode = "Test",
+                PlanDescription = "Test",
+                NewUsed = "Test",
+                OdometerType = "Test",
+                ExpireOdometer = "Test",
+                OwnerFirstName = "Test",
+                OwnerLastName = "Test"
+
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "Covered Item [ ItemType=type, ItemId=id ]\r\n";
+            var expected = "Covered Item [ ItemType=type, ItemId=id, Manufacturer=Test, Model=Test, BookStateOrProvince=Test, PostalCode=Test, PlanCode=Test, PlanDescription=Test, NewUsed=Test, OdometerType=Test, ExpireOdometer=Test, OwnerLastName=Test, OwnerFirstName=Test ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -392,14 +449,23 @@ namespace VPay.Payment.Common.Tests
             var entity = new SwitchTransactionData
             {
                 AcquireID = "id",
-                AuthCode = "auth"
+                AuthCode = "auth",
+                AvailableBal = "200",
+                CaptureTS = "2023-01-01",
+                CurrentBal = "100",
+                MerchantID = "id",
+                OlsLogID = "id",
+                Stan = "stan",
+                Switch = "switch",
+                TerminalID = "id",
+                TransactionTS = "2023-01-01"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "SwitchTransaction [ AcquireID=id, AuthCode=auth ]\r\n";
+            var expected = "SwitchTransaction [ AcquireID=id, AuthCode=auth, AvailableBal=200, CaptureTS=2023-01-01, CurrentBal=100, MerchantID=id, OlsLogID=id, Stan=stan, Switch=switch, TerminalID=id, TransactionTS=2023-01-01 ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -429,14 +495,30 @@ namespace VPay.Payment.Common.Tests
             var entity = new HeaderData
             {
                 TransNumber = 123,
-                Client = "client"
+                Client = "client",
+                BillCode = "code",
+                BillType = "type",
+                AvailBalance = "200",
+                CurrentBalance = "100",
+                SwitchAvailBal = "200",
+                SwitchCurrentBal = "100",
+                PayeeCode = "code",
+                PayeeName = "name",
+                ProviderName = "provname",
+                RequesterId = "id",
+                RequesterName = "name",
+                TaxId = "id",
+                UserField1 = "userfield1",
+                UserField2 = "userfield2",
+                UserField3 = "userfield3"
+
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "HeaderData [ TransNumber=123, Client=client ]\r\n";
+            var expected = "HeaderData [ TransNumber=123, Client=client, BillCode=code, BillType=type, AvailBalance=200, CurrentBalance=100, SwitchAvailBal=200, SwitchCurrentBal=100, PayeeCode=code, PayeeName=name, ProviderName=provname, RequesterId=id, RequesterName=name, TaxId=*****, UserField1=userfield1, UserField2=userfield2, UserField3=userfield3 ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -447,14 +529,20 @@ namespace VPay.Payment.Common.Tests
             var entity = new PayTypeDetail
             {
                 Association = "assoc",
-                Bank = "bank"
+                Bank = "bank",
+                CardCvv2 = "123",
+                CardExp = "12/2023",
+                OutsideCheck = "check",
+                ClearCheck = "clearcheck",
+                PosPayCheck = "pospaycheck",
+                SwitchNumber = "switch"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "PayTypeDetail [ Association=assoc, Bank=bank ]\r\n";
+            var expected = "PayTypeDetail [ Association=assoc, Bank=bank, CardCvv2=1***, CardExp=12/2023, OutsideCheck=check, ClearCheck=clearcheck, PosPayCheck=pospaycheck, SwitchNumber=switch ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -465,14 +553,29 @@ namespace VPay.Payment.Common.Tests
             var entity = new Detail
             {
                 TranId = 1,
-                LoadTran = 52
+                LoadTran = 52,
+                Status = "status",
+                Amount = "200",
+                AuthCode = "auth",
+                TranTimeStamp = "2023-01-01",
+                Expiration = "12/2023",
+                MerchantCode = "code",
+                MerchantName = "name",
+                ReasonCode = "001",
+                ReasonDesc = "desc",
+                ActionCode = "code",
+                ActionDesc = "desc",
+                FinancialType = "type",
+                RequesterName = "name",
+                BatchNumber = "123",
+                StatusDesc = "statusdesc"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "Detail [ TranId=1, LoadTran=52 ]\r\n";
+            var expected = "Detail [ TranId=1, LoadTran=52, Status=status, Amount=200, AuthCode=auth, TranTimeStamp=2023-01-01, Expiration=12/2023, MerchantCode=code, MerchantName=name, ReasonCode=001, ReasonDesc=desc, ActionCode=code, ActionDesc=desc, FinancialType=type, RequesterName=name, BatchNumber=123, StatusDesc=statusdesc ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -483,14 +586,32 @@ namespace VPay.Payment.Common.Tests
             var entity = new CorespDtl
             {
                 Direction = "dir",
-                Type = "type"
+                Type = "type",
+                Status = "status",
+                SentBehalfName = "name",
+                FromName = "fromName",
+                FromAddress1 = "fromaddress1",
+                FromAddress2 = "fromaddress2",
+                FromCity = "fromcity",
+                FromState = "fromstate",
+                FromFax = "fromfax",
+                FromPostalCode = "frompostal",
+                ToName = "toName",
+                ToAddress1 = "toaddress1",
+                ToAddress2 = "toaddress2",
+                ToCity = "tocity",
+                ToState = "tostate",
+                ToPostalCode = "topostal",
+                ToFax = "tofax",
+                ToPhone = "tophone",
+                StatusText = "statusText"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "Correspondence [ Direction=dir, Type=type, RequestDate=0, StatusDate=0, DmRecId=0 ]\r\n";
+            var expected = "Correspondence [ Direction=dir, Type=type, Status=status, RequestDate=0, StatusDate=0, SentBehalfName=name, FromName=fromName, FromAddress1=fromaddress1, FromAddress2=fromaddress2, FromCity=fromcity, FromState=fromstate, FromPostalCode=frompostal, FromFax=fromfax, ToName=toName, ToAddress1=toaddress1, ToAddress2=toaddress2, ToCity=tocity, ToState=tostate, ToPostalCode=topostal, ToFax=tofax, ToPhone=tophone, StatusText=statusText, DmRecId=0 ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -501,7 +622,8 @@ namespace VPay.Payment.Common.Tests
             var request = new TradingPostData.LoadRequest
             {
                 CardHolder = new TradingPostData.Cardholder { Client = "client" },
-                Merchant = new TradingPostData.Merchant { PayeeCode = "code" }
+                Merchant = new TradingPostData.Merchant { PayeeCode = "code" },
+
             };
 
             // Act
@@ -645,14 +767,29 @@ namespace VPay.Payment.Common.Tests
             var entity = new TradingPostData.CoveredItem
             {
                 ItemType = "type",
-                ItemId = "id"
+                ItemId = "id",
+                Year = "2023",
+                Manufacturer = "Test",
+                Model = "model",
+                BookStateOrProvince = "bookstate",
+                PostalCode = "postal",
+                PlanCode = "plan",
+                PlanDescription = "desc",
+                Deductible = "deductible",
+                NewUsed = "new",
+                BeginDate = "03/01/2025",
+                ExpireDate = "03/05/2025",
+                OdometerType = "odometer",
+                ExpireOdometer = "expire",
+                OwnerFirstName = "first",
+                OwnerLastName = "last"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "Covered Item [ ItemType=type, ItemId=id ]\r\n";
+            var expected = "Covered Item [ ItemType=type, ItemId=id, ItemYear=2023, Manufacturer=Test, Model=model, BookStateOrProvince=bookstate, PostalCode=postal, PlanCode=plan, PlanDescription=desc, Deductible=deductible, NewUsed=new, BeginDate=03/01/2025, ExpireDate=03/05/2025, OdometerType=odometer, ExpireOdometer=expire, OwnerLastName=last, OwnerFirstName=first ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -663,14 +800,24 @@ namespace VPay.Payment.Common.Tests
             var entity = new TradingPostData.Claim
             {
                 UserKey = "userKey",
-                UserField1 = "field1"
+                UserField1 = "field1",
+                UserField2 = "field2",
+                UserField3 = "field3",
+                CurrencyType = "840",
+                Amount = "200",
+                ClaimDeductible = "deductible",
+                ClaimOdometer = "odometer",
+                ClaimDescription = "desc",
+                RequesterName = "name",
+                RepairOrderId = "orderId",
+                ClaimNotes = "notes"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "Claim [ UserKey=userKey, UserField1=field1 ]\r\n";
+            var expected = "Claim [ UserKey=userKey, UserField1=field1, UserField2=field2, UserField3=field3, CurrencyType=840, Amount=200, ClaimDeductible=deductible, ClaimOdometer=odometer, ClaimDescription=desc, RequesterName=name, RepairOrderId=orderId, ClaimNotes=notes ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -699,14 +846,21 @@ namespace VPay.Payment.Common.Tests
             var entity = new TradingPostData.TransactionInformation
             {
                 ResponseCode = "code",
-                ResponseDescription = "desc"
+                ResponseDescription = "desc",
+                StatusCode = "code",
+                StatusDescription = "desc",
+                TransactionId = "id",
+                AuditNumber = "number",
+                TransactionTime = "time",
+                AccountingCode = "code",
+                AccountingDescription = "desc"
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "TransactionInformation [ ResponseCode=code, ResponseDescription=desc ]\r\n";
+            var expected = "TransactionInformation [ ResponseCode=code, ResponseDescription=desc, StatusCode=code, StatusDescription=desc, TransactionId=id, AuditNumber=number, TransactionTime=time, AccountingCode=code, AccountingDescription=desc ]\r\n";
             Assert.Equal(expected, result);
         }
 
@@ -719,19 +873,21 @@ namespace VPay.Payment.Common.Tests
                 CardType = "Visa",
                 CardNumber = "1234567890123456",
                 CardSecurityValue = "123",
-                CardExpiration = "12/2023"
+                CardExpiration = "12/2023",
+                PayeeName = "name",
+                CardholderName = "name",
+                CardholderAddress = "address",
+                CardholderPostalCode = "postal",
+
             };
 
             // Act
             var result = entity.ToDisplayString();
 
             // Assert
-            var expected = "CardInformation [ CardType=Visa, CardNumber=1234*********56, CardCvv2=***, CardExpiration=12/2023 ]\r\n";
+            var expected = "CardInformation [ CardType=Visa, CardNumber=1234*********56, CardCvv2=***, CardExpiration=12/2023, PayeeName=name, CardholderName=name, CardholderAddress=address, CardholderPostalCode=postal ]\r\n";
             Assert.Equal(expected, result);
         }
     }
 }
 
-// Additional tests for other methods can be added similarly
-//}
-//}
